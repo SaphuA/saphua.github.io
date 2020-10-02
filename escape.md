c2657a7f33751bd2ecb77fdce568b47ecd68e0ba
t.hendriks <tijs.hendriks@isatis-health.nl>
2020-10-02 13:08:03 +0200

    Initial commit

---
+++ escape.md
@@ -0,0 +1,84 @@
+# Introduction
+
+You will need:
+
+- Scissors
+- Glue
+- At least two phones
+- A laptop with an internet connection and updated browser.
+
+# A letter
+
+Dear reader,
+
+My name is [name]. I am writing on behalf of [organization].
+
+Recent events have led me to believe that our organization has been compromised. My colleagues are no longer to be trusted. This is why I am seeking help from the outside.
+
+I have been following you for a while now and you are a very suitable candidate.
+
+Unfortunately, upon receiving this letter you too are in danger. I am sorry. There was no other way.
+
+Leave for the airport as quickly as possible. Hurry. Bring only your phones and a laptop. Read the attached letter while you're on your way.
+
+[signature]
+
+---
+
+---
+
+# Attached letter
+
+I hope you managed to get on your way safely. When you arrive go to the second floor. There's a storage room behind the kiosk containing many lockers: [url]
+
+I trust that you have everything you need to find and open the right locker.
+
+Avoid camera's and trust no-one. I will be in contact soon.
+
+[signature]
+
+---
+
+---
+
+[dice]]
+
+---
+
+---
+
+Pagina-einde
+
+In de officieel niet-bestaande hangar #13 staat jullie een vliegtuig te wachten. De toegang tot de hangar zal eerst verkregen moeten worden via het controlepaneel in de controlekamer. Dit is niet voor iedereen weggelegd. De controlekamer is volledig afgesloten voor het publiek. Een van jullie zal via de luchtschacht toegang moeten proberen te krijgen. De ander zal in het oude kantoor om de hoek op zoek moeten gaan naar instructies.
+
+•Geef de brief “controlekamer” aan degene die achterblijft met een laptop en telefoon.
+
+•Geef de brief “kantoor” aan degene die naar een andere kamer gaat met een telefoon.
+
+N.B. Lees de brieven niet voordat jullie elk in een andere kamer zijn.
+
+Pagina-einde
+
+Dankzij jouw flexibiliteit beland je in de controlekamer. Start het controlepaneel op om in de interface te komen: http://tiny.cc/CTRLPNL. Omdat de verbinding hier niet beveiligd is, is het noodzakelijk om enkel met je partner te communiceren via emoji’s. Stuur een duimpje omhoog als je zover bent.
+
+Als de machine is opgestart zullen automatisch de fabrieksinstellingen getoond worden. Als je partner erin slaagt de juiste configuratie te vinden zal je deze spoedig ontvangen. Denk aan jullie veiligheid en gebruik enkel emoji’s!
+
+C:\Users\tijs.hendriks\AppData\Local\Microsoft\Windows\INetCache\Content.Word\controlepaneel_start.png
+
+Pagina-einde
+
+Mooi, je bent in het kantoor. Je vindt de klapper met instructies snel. Helaas zit deze vastgeketend aan de dossierkast. Je zult de instructies met je telefoon moeten doorsturen. Omdat de verbinding hier niet beveiligd is, is het noodzakelijk om enkel met je partner te communiceren via emoji’s. Stuur een duimpje omhoog als je zover bent.
+
+In de klapper vind je voorin de fabrieksinstellingen. Dit zijn de instellingen die de gebruiker ziet bij het opstarten van het controlepaneel.
+
+C:\Users\tijs.hendriks\AppData\Local\Microsoft\Windows\INetCache\Content.Word\controlepaneel_start.png
+
+Een aantal pagina’s verder vind je de instellingen om toegang te krijgen tot hangar #13. Je doel is om deze te communiceren aan je partner, maar denk aan jullie veiligheid en gebruik enkel emoji’s!
+
+Pagina-einde
+
+In de hangar staat een klein vliegtuig te wachten. Er is in de omgeving verder niemand te bekennen. In het vliegtuig knippert een dashboard dat lijkt op een soort gps-systeem: http://tiny.cc/HNG13. Er wordt gevraagd om een bestemming in te voeren.
+
+In een dashboardkastje liggen wat oude vliegtickets en een wereldkaart.
+
+Pagina-einde
---
+++ recipes/main.js
@@ -0,0 +1,165 @@
+window.addEventListener("load", function () {
+  Vue.use(VueRouter);
+
+  var router = new VueRouter({
+    routes: [{ path: "/:groupId" }],
+  });
+
+  var app = new Vue({
+    directives: {
+      tooltip: tooltip,
+    },
+    router: router,
+    components: {
+      Toolbar: toolbar,
+      Slider: slider,
+      Dropdown: dropdown,
+      Dialog2: dialog,
+      Button2: button,
+    },
+    el: "#app",
+    data: {
+      pv: "",
+      ac: false,
+      localImages: false,
+      favorites: JSON.parse(window.localStorage.getItem("favorites") || "[]"),
+
+      hfCuisinesNormalized: _hfCuisinesNormalized,
+      hfIngredientsNormalized: _hfIngredientsNormalized,
+      hfRecipeVersions: _hfRecipeVersions,
+      hfTagsNormalized: _hfTagsNormalized,
+
+      prepTimesMin: 0,
+      prepTimesMax: 140,
+      selectedPrepTimesRange: [0, 140],
+
+      complexityMin: 0,
+      complexityMax: 3,
+      selectedComplexities: [0, 3],
+
+      selectedCuisine: null,
+      selectedTag: null,
+      selectedIngredient: null,
+
+      onlyFavorites: false,
+
+      selectedRecipeVersions: null,
+      selectedRecipeIndex: 0,
+      selectedYield: 1,
+    },
+    methods: {
+      ps: function () {
+        if (btoa(this.pv) == "YXBwZWx0YWFydA==") {
+          this.ac = true;
+        }
+      },
+      navigateToRecipe: function (recipe) {
+        router.push({ query: { groupId: recipe.groupId } });
+      },
+      closeModal: function () {
+        router.push({ query: null });
+      },
+      selectRecipeVersions: function (recipeVersions) {
+        this.selectedRecipeVersions = recipeVersions;
+        this.selectedRecipeIndex = 0;
+        this.selectedYield = recipeVersions[0].yields[0].yields;
+      },
+      selectRecipeVersionIndex: function (index) {
+        if (!this.hasYield(this.selectedRecipeVersions[index], this.selectedYield)) {
+          this.selectedYield = this.selectedRecipeVersions[index].yields[0].yields;
+        }
+        this.selectedRecipeIndex = index;
+      },
+      selectYield: function (yield) {
+        this.selectedYield = yield;
+      },
+      hasYield: function (recipe, yield) {
+        if (!recipe.yields) return false;
+
+        for (var i = 0; i < recipe.yields.length; i++) {
+          if (recipe.yields[i].yields == yield) {
+            return true;
+          }
+        }
+
+        return false;
+      },
+      getYield: function (recipe, ingredient, yield) {
+        if (!recipe.yields) return null;
+
+        for (var i = 0; i < recipe.yields.length; i++) {
+          if (recipe.yields[i].yields == yield) {
+            for (var j = 0; j < recipe.yields[i].ingredients.length; j++) {
+              if (recipe.yields[i].ingredients[j].id == ingredient) {
+                return recipe.yields[i].ingredients[j];
+              }
+            }
+          }
+        }
+
+        return null;
+      },
+      toggleFavorite: function (recipe) {
+        if (this.favorites.includes(recipe.groupId)) {
+          this.favorites = this.favorites.filter((f) => f != recipe.groupId);
+        } else {
+          this.favorites.push(recipe.groupId);
+        }
+
+        window.localStorage.setItem("favorites", JSON.stringify(this.favorites));
+      },
+    },
+    computed: {
+      selectedRecipe: function () {
+        if (!this.selectedRecipeVersions) return null;
+        return this.selectedRecipeVersions[this.selectedRecipeIndex];
+      },
+      hfCuisines: function () {
+        return Object.keys(this.hfCuisinesNormalized).map((key) => this.hfCuisinesNormalized[key]);
+      },
+      hfIngredients: function () {
+        return Object.keys(this.hfIngredientsNormalized).map((key) => this.hfIngredientsNormalized[key]);
+      },
+      hfRecipeVersionsFiltered: function () {
+        return this.hfRecipeVersions
+          .map((v) =>
+            v.filter(
+              (r) =>
+                r.prepTimeMinutes >= this.selectedPrepTimesRange[0] &&
+                r.prepTimeMinutes <= this.selectedPrepTimesRange[1] &&
+                r.difficulty >= this.selectedComplexities[0] &&
+                r.difficulty <= this.selectedComplexities[1] &&
+                (this.selectedCuisine == null || r.cuisines.some((c) => c.id === this.selectedCuisine.id)) &&
+                (this.selectedTag == null || r.tags.some((c) => c.id === this.selectedTag.id)) &&
+                (this.selectedIngredient == null || r.ingredients.some((c) => c.id === this.selectedIngredient.id)) &&
+                (!this.onlyFavorites || this.favorites.some((f) => f === r.groupId))
+              //(this.selectedCuisines.length === 0 || (r.cuisines && this.selectedCuisines.every((c) => r.cuisines.some((s) => s.id === c.id)))) &&
+              //(this.selectedTags.length === 0 || (r.tags && this.selectedTags.every((c) => r.tags.some((s) => s.id === c.id)))) &&
+              //(this.selectedIngredients.length === 0 || (r.ingredients && this.selectedIngredients.every((c) => r.ingredients.some((s) => s.id === c.id))))
+            )
+          )
+          .filter((v) => v && v.length > 0);
+      },
+      hfTags: function () {
+        return Object.keys(this.hfTagsNormalized).map((key) => this.hfTagsNormalized[key]);
+      },
+    },
+    watch: {
+      $route: {
+        immediate: true,
+        handler(to) {
+          if (to.query && to.query.groupId) {
+            this.selectedRecipeVersions = this.hfRecipeVersions.find((c) => c[0].groupId == to.query.groupId);
+            this.selectedRecipeIndex = 0;
+            this.selectedYield = this.selectedRecipeVersions[0].yields[0].yields;
+          } else {
+            this.selectedRecipeVersions = null;
+            this.selectedRecipeIndex = 0;
+            this.selectedYield = 1;
+          }
+        },
+      },
+    },
+  });
+  Vue.config.devtools = true;
+});
